849888ec5a70a8e0472e00244666a0865c783fd9
LiuSeeker <vitor.liu@gmail.com>
2019-11-25 15:12:17 -0300

    fim

--- webserver.py
+++ webserver.py
@@ -8,88 +8,90 @@ app = Flask(__name__, static_url_path="")
 api = Api(app)
 
 tasks = [
-    {
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
+	{
+		'title': u'Buy groceries',
+		'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+		'done': False
+	},
+	{
+		'title': u'Learn Python',
+		'description': u'Need to find a good Python tutorial on the web',
+		'done': False
+	}
 ]
 
 task_fields = {
-    'title': fields.String,
-    'description': fields.String,
-    'done': fields.Boolean,
-    'uri': fields.Url('tasks')
+	'title': fields.String,
+	'description': fields.String,
+	'done': fields.Boolean,
+	'uri': fields.Url('tasks')
 }
 
 if len(sys.argv) > 1:
-    public_ip = str(sys.argv[1])
+	public_ip = str(sys.argv[1])
 
 
 class TaskListAPI(Resource):
 
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type=str, required=True,
-                                   help='No task title provided',
-                                   location='json')
-        self.reqparse.add_argument('description', type=str, default="",
-                                   location='json')
-        super(TaskListAPI, self).__init__()
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('title', type=str, required=True,
+								   help='No task title provided',
+								   location='json')
+		self.reqparse.add_argument('description', type=str, default="",
+								   location='json')
+		super(TaskListAPI, self).__init__()
 
-    def get(self):
-        res = requests.get(url="http://"+public_ip+":5000/tasks").json()
-        return res.json()
+	def get(self):
+		res = requests.get(url="http://"+public_ip+":5000/tasks")
+		return res.json()
 
-    def post(self):
-        args = self.reqparse.parse_args()
-        task = {
-            'title': args['title'],
-            'description': args['description']
-        }
-        res = requests.post(url="http://"+public_ip+":5000/tasks", data=json.dumps(task))
-        return res.json()
+	def post(self):
+		args = self.reqparse.parse_args()
+		task = {
+			'title': args['title'],
+			'description': args['description']
+		}
+		print(task)
+		res = requests.post(url="http://"+public_ip+":5000/tasks", json=task)
+		return res.json()
 
 class TaskAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type=str, location='json')
-        self.reqparse.add_argument('description', type=str, location='json')
-        self.reqparse.add_argument('done', type=bool, location='json')
-        super(TaskAPI, self).__init__()
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('title', type=str, location='json')
+		self.reqparse.add_argument('description', type=str, location='json')
+		self.reqparse.add_argument('done', type=bool, location='json')
+		super(TaskAPI, self).__init__()
 
-    def get(self, id):
-        res = requests.get(url="http://"+public_ip+":5000/tasks/"+id)
-        return res.json()
+	def get(self, id):
+		res = requests.get(url="http://"+public_ip+":5000/tasks/"+id)
+		return res.json()
 
-    def put(self, id):
-        args = self.reqparse.parse_args()
-        task = {
-            'title': args['title'],
-            'description': args['description'],
-			'done': args['done']
-        }
-        res = requests.post(url="http://"+public_ip+":5000/tasks/"+id, data=json.dumps(task))
-        return res.text
-
-    def delete(self, id):
+	def put(self, id):
 		args = self.reqparse.parse_args()
-        task = {
-            'title': args['title'],
-            'description': args['description'],
+		task = {
+			'title': args['title'],
+			'description': args['description'],
 			'done': args['done']
-        }
-        res = requests.delete(url="http://"+public_ip+":5000/tasks/"+id, data=json.dumps(task))
-        return res.text
+		}
+		res = requests.put(url="http://"+public_ip+":5000/tasks/"+id, json=task)
+		return res.json()
+
+	def delete(self, id):
+		args = self.reqparse.parse_args()
+		task = {
+			'title': args['title'],
+			'description': args['description'],
+			'done': args['done']
+		}
+		res = requests.delete(url="http://"+public_ip+":5000/tasks/"+id, json=task)
+		return res.json()
 
 class HealthCheck(Resource):
-    def get(self):
-        return requests.get(url="http://"+public_ip+":5000/healthcheck")
+	def get(self):
+		res = requests.get(url="http://"+public_ip+":5000/healthcheck")
+		return res.json()
 
 api.add_resource(TaskListAPI, '/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/tasks/<id>', endpoint='task')
@@ -97,4 +99,4 @@ api.add_resource(HealthCheck, "/healthcheck", endpoint="healthcheck")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+	app.run(debug=True, host="0.0.0.0", port=5000)
